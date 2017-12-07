11f053a93156922b1818ac6875be4f0c5714a36d
benedemon <rajsingh1996@gmail.com>
2017-12-07 12:43:30 +0530

    All APIs

--- src/routes/deleteLink.js
+++ src/routes/deleteLink.js
@@ -1,40 +1,17 @@
 const deleteLink = require('express').Router();
-import { readPool, writePool } from '../db';
-// const bcrypt = require('bcrypt');
+import { writePool } from '../db';
 const sendResponse = require('../helpers/sendResponse');
-//const jwt = require('jsonwebtoken');
-
-deleteLink.get('/', async (req, res) => {
-    //const { title } = req.query;
-    try {
-      const [result] = await readPool.query('SELECT * FROM links');
-      res.json(result);
-    } catch (error) {
-      console.error(error)
-    }
-  });
 
 deleteLink.route('/').post(async (req, res) => {
   try {
-    console.log('abbaaa');
-    console.log(req.query);
+    const id = req.query.id;
 
-    const { userId, link } = req.query;
-
-    const linkDetails = {
-      userId,
-      link,
-    };
-
-    const [newLink] = await writePool.query(`Delete from links where link = '${link}' `);
+    const [newLink] = await writePool.query(`Delete from links where id = '${id}' `);
 
     res.status(200).json(newLink);
   } catch (err) {
     console.error(err);
-    if (err.code === 'ER_DUP_ENTRY') {
-      return sendResponse(res, 409, [], 'email/username already exist');
-    }
-    return sendResponse(res, 500, [], 'failed', 'something went wrong');
+    res.status(500).send('Something went wrong');
   }
 });
 
